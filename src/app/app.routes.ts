00a6300d864dfa7cb750655747b5a90dd9e8eb2c
test@gmail.com <test@gmail.com>
2026-03-24 01:15:01 +0530

    icons and design updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,15 @@ export const routes: Routes = [
         pathMatch: 'full'
     },
     {
-        path:'businesses',
-        loadComponent : () => import('./components/business-list/business-list.component').then(m => m.BusinessListComponent)
+        path: 'businesses',
+        loadComponent: () => import('./components/business-list/business-list.component').then(m => m.BusinessListComponent)
     },
     {
-        path:'business-details',
-        loadComponent : () => import('./components/business-detail/business-detail.component').then(m => m.BusinessDetailComponent)
+        path: 'search',
+        loadComponent: () => import('./components/search-business/search-business.component').then(m => m.SearchBusinessComponent)
+    },
+    {
+        path: 'business-details',
+        loadComponent: () => import('./components/business-detail/business-detail.component').then(m => m.BusinessDetailComponent)
     }
 ];
--- src/app/services/transfer-state.service.ts
+++ src/app/services/transfer-state.service.ts
@@ -10,7 +10,6 @@ export class TransferStateService {
 
     get<T>(key: string) {
         const stateKey = makeStateKey<T>(key);
-
         if (this.transferState.hasKey(stateKey)) {
             return this.transferState.get(stateKey, null as any);
         }
